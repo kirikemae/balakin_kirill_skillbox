8dc0b0e2f37f17a49617903867857904559fd464
kirikemae <128600909+kirikemae@users.noreply.github.com>
2024-05-02 23:37:27 +0500

    mod13

---
+++ mod 13/task1 (3).py
@@ -0,0 +1,31 @@
+import sqlite3
+
+
+get_temp = """
+SELECT temperature_in_celsius
+FROM table_truck_with_vaccine
+WHERE truck_number = ?
+"""
+
+def check_if_vaccine_has_spoiled(
+        cursor: sqlite3.Cursor,
+        truck_number: str
+) -> bool:
+    cursor.execute(get_temp, (truck_number,))
+
+    result = cursor.fetchone()
+    if result is not None:
+        if 18 <= result[0] <= 20:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    with sqlite3.connect("hw1.db") as conn:
+        c=conn.cursor()
+        n = input('Введите номер грузовика')
+
+        print(check_if_vaccine_has_spoiled(c, n))
---
+++ mod 13/task3.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+create_table_query = """
+create table if not exists birds (
+    bird_name text primary key,
+    date_time text
+)
+"""
+
+get_bird = """
+select date_time
+from birds
+where bird_name = ?
+"""
+
+def log_bird(
+        cursor: sqlite3.Cursor,
+        bird_name: str,
+        date_time: str,
+):
+    cursor.execute("insert into birds (bird_name, date_time) values (?, ?)", (bird_name, date_time))
+    cursor.connection.commit()
+
+
+def check_if_such_bird_already_seen(
+        cursor: sqlite3.Cursor,
+        bird_name: str
+) -> bool:
+    cursor.execute(get_bird, (bird_name, ))
+    result=cursor.fetchone()
+    if result is not None:
+        return True
+    else:
+        return False
+
+if __name__=='__main__':
+    with sqlite3.connect('birds_saw.db') as conn:
+        c = conn.cursor()
+        c.execute(create_table_query)
+        a=input('введите название птицы которую вы видели')
+        b=input('введите дату когда вы ее видели(месяц в формате "00")')
+        log_bird(c, a, b)
+        n=input('введите имя птицы')
+        print(check_if_such_bird_already_seen(c, n))
